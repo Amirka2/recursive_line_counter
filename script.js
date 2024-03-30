fa4ca7d4f2c417663bbae23420d16a8befb25be1
Amir Fayzullin <far20022002@mail.ru>
2024-03-31 00:23:29 +0500

    feat: renamed func getFilePathsArray, added countFileLines, getTotalCount

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ import { createReadStream, statSync, readdirSync } from "fs";
 
 const pathRoot = '../shop/src';
 
-function getFiles(dir, files = []) {
+function getFilePathsArray(dir, files = []) {
     const fileList = readdirSync(dir)
 
     for (const file of fileList) {
         const name = `${dir}/${file}`
         if (statSync(name).isDirectory()) {
-            getFiles(name, files)
+            getFilePathsArray(name, files)
         } else {
             files.push(name)
         }
@@ -17,3 +17,32 @@ function getFiles(dir, files = []) {
     return files
 }
 
+const countFileLines = function(filePath, callback) {
+    let i;
+    let count = 0;
+
+    return createReadStream(filePath)
+        .on('error', e => callback(e))
+        .on('data', chunk => {
+            for (i=0; i < chunk.length; ++i) if (chunk[i] === 10) count++;
+        })
+        .on('end', () => callback(null, count));
+};
+
+
+const getTotalCount = function (directory) {
+    const files = getFilePathsArray(directory);
+    let totalCount = 0;
+
+    files.forEach((file) => {
+        countFileLines(file, (error, count) => {
+            // console.log(count);
+            // return count;
+            totalCount += count;
+        })
+    })
+    return totalCount;
+}
+
+const count = getTotalCount(pathRoot)
+console.log(count);
